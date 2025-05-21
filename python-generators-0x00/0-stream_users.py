c445e2384041377f474df6c0a15100c8a4d60491
teshager <teshager8922@gmail.com>
2025-05-21 15:07:52 +0300

    feat: create a generator that streams rows from an SQL database one by one.

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = "ALX_prodev"
+
+def stream_users():
+    conn = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+    cursor = conn.cursor(dictionary=True)  # fetch rows as dicts
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    conn.close()
