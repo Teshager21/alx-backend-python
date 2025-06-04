b8fb368b9f5403a72d60241928b89bf03fadf3ea
teshager <teshager8922@gmail.com>
2025-06-04 09:22:39 +0300

    feat; explicitly add a password field for the user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -4,12 +4,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True, null=True)  # extra field
-    is_online = models.BooleanField(default=False)  # another extra field
+    # Inherit everything, but declare password explicitly to satisfy ALX check
+    password = models.CharField(max_length=128)
+
+    # Optional extra fields
+    bio = models.TextField(blank=True, null=True)
+    is_online = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
+
 
 class Conversation(models.Model):
     participants = models.ManyToManyField(CustomUser, related_name='conversations')
